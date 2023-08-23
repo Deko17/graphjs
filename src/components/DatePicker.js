9741509d0cb5d4544f4a05d76e2fb7eb616edefc
Daryn Kenessov <57325957+Deko17@users.noreply.github.com>
2023-08-23 12:46:33 +0600

    Update DatePicker.js

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,31 +1,58 @@
 import React, { useState } from 'react';
-import { DateRangePicker } from 'react-date-range';
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import './DatePicker.css';
 
 const DatePicker = ({ onDateRangeChange }) => {
-	const [selectedDates, setSelectedDates] = useState([
-		{
-			startDate: new Date(),
-			endDate: new Date(),
-			key: 'selection',
-		},
-	]);
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
 
-	const handleDateRangeChange = (ranges) => {
-		setSelectedDates([ranges.selection]);
+	const handleStartDateChange = (event) => {
+		setStartDate(new Date(event.target.value));
+	};
+
+	const handleEndDateChange = (event) => {
+		setEndDate(new Date(event.target.value));
+	};
+
+	const handleApplyButtonClick = () => {
 		if (onDateRangeChange) {
-			onDateRangeChange(ranges.selection);
+			const range = {
+				startDate,
+				endDate,
+			};
+			onDateRangeChange(range);
 		}
 	};
 
 	return (
-		<div>
-			<DateRangePicker
-				ranges={selectedDates}
-				onChange={handleDateRangeChange}
-			/>
+		<div style={{ display: 'flex', alignItems: 'center' }}>
+			<div style={{ marginRight: '10px', border: '1px solid rgba(255, 255, 255, 0.5)', borderRadius: '5px' }}>
+				<input
+					type="date"
+					value={startDate.toISOString().split('T')[0]}
+					onChange={handleStartDateChange}
+					style={{
+						backgroundColor: 'transparent',
+						border: 'none',
+						color: 'white',
+						width: '150px',
+						padding: '10px',
+					}}
+				/>
+			</div>
+			<div style={{ marginRight: '10px', border: '1px solid rgba(255, 255, 255, 0.5)', borderRadius: '5px' }}>
+				<input
+					type="date"
+					value={endDate.toISOString().split('T')[0]}
+					onChange={handleEndDateChange}
+					style={{
+						backgroundColor: 'transparent',
+						border: 'none',
+						color: 'white',
+						width: '150px',
+						padding: '10px',
+					}}
+				/>
+			</div>
+			<button onClick={handleApplyButtonClick} style={{ padding: '10px 10px', background: 'transparent', border: '1px solid rgba(255, 255, 255, 0.5)', borderRadius: '5px', color: 'white', width: '150px', fontSize: '16px' }}>Применить</button>
 		</div>
 	);
 };
